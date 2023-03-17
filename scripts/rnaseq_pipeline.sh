6bfe1756f001ffa8c795c34222ea504753d38982
esme-mar <122739842+esme-mar@users.noreply.github.com>
2023-03-17 15:02:02 -0500

    Update rnaseq_pipeline.sh

--- scripts/rnaseq_pipeline.sh
+++ scripts/rnaseq_pipeline.sh
@@ -29,7 +29,7 @@ echo "Trimmomatic finished running!"
 
 ## for multiple SRAs downloaded in file SRR_Acc_List.txt 
 # (cat SRR_Acc_List.txt | parallel -j 4 "trimmomatic PE -threads 4 -trimlog trimmedReads/{}trimming.log sra_data/{}_1.fastq sra_data/{}_2.fastq -baseout
-trimmed_reads/SRR968437 ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLENGTH:36"
+# trimmed_reads/SRR968437 ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLENGTH:36"
 
  
 /data/mcfadden/smoaleman/esme/sra_data/*.fastq /data/mcfadden/smoaleman/esme/trimmed_reads
