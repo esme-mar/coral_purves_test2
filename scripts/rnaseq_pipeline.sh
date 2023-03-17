0a8e71376241f9359cbfea9219378991255f65df
esme-mar <122739842+esme-mar@users.noreply.github.com>
2023-03-17 15:00:57 -0500

    Update rnaseq_pipeline.sh

--- scripts/rnaseq_pipeline.sh
+++ scripts/rnaseq_pipeline.sh
@@ -1,14 +1,14 @@
 # download files from SRA, run fastqc, trim reads,
-# set working directory
 
+# set working directory
  cd /data/mcfadden/aquattrini/PROGRAMS/sratoolkit.3.0.2-ubuntu64/bin
 
- ./fasterq-dump SRR9648437 -O /data/mcfadden/smoaleman/esme/sra_data
-
-#didn't work: fastqc -t 12 sra_data/*.fastq -o /sra_data 
+ ./fasterq-dump SRR9648437 -O /data/mcfadden/smoaleman/esme/sra_data 
 
  #run fastqc
  fastqc -t 12 sra_data/*.fastq -o /data/mcfadden/smoaleman/esme/sra_data
+ 
+ #didn't work: fastqc -t 12 sra_data/*.fastq -o /sra_data
 
  #download *fastqc.zip files to home desktop 
 scp smoaleman@purves.cs.hmc.edu:/data/mcfadden/smoaleman/esme/sra_data/SRR9648437_*_fastqc.zip 
@@ -16,11 +16,14 @@ scp smoaleman@purves.cs.hmc.edu:/data/mcfadden/smoaleman/esme/sra_data/SRR964843
 # SRR9648437_*_fastqc didn't work, had to put _1_ and _2_
 
 
-mkdir trimmed_reads
+#mkdir trimmed_reads
 
- # run trimmomatic to trim reads with poor quality
-java -jar /data/mcfadden/aquattrini/PROGRAMS/trimmomatic-0.35/trimmomatic-0.35.jar PE -threads 4 sra_data/SRR9648437_1.fastq sra_data/SRR9648437_2.fastq -baseout 
-trimmed_reads/SRR968437 ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLENGTH:36 
+gz *fastq
+
+# run trimmomatic to trim reads with poor quality
+java -jar /data/mcfadden/aquattrini/PROGRAMS/trimmomatic-0.35/trimmomatic-0.35.jar PE -threads 4 SRR9648437_1.fastq SRR9648437_2.fastq 
+/data/mcfadden/smoaleman/esme/trimmed_reads/SRR9648437_R1_PE_trimmed.fastq.gz /data/mcfadden/smoaleman/esme/trimmed_reads/SRR9648437_R1_SE_trimmed.fastq.gz 
+/data/mcfadden/smoaleman/esme/trimmed_reads/SRR9648437_R2_PE_trimmed.fastq.gz /data/mcfadden/smoaleman/esme/trimmed_reads/SRR9648437_R2_SE_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLENGTH:36 
 
 echo "Trimmomatic finished running!"
 
